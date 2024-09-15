45146427c2c7651c38b220a2195acc9a8b0997e4
lapkomo2018 <lapkomo2018@gmail.com>
2024-09-15 19:01:45 +0300

    added grpcutil

---
+++ internal/grpcutil/grpcutil.go
@@ -0,0 +1,19 @@
+package grpcutil
+
+import (
+	"context"
+	"github.com/lapkomo2018/goTwitterAuthService/pkg/discovery"
+	"google.golang.org/grpc"
+	"math/rand"
+)
+
+// ServiceConnection returns a connection to a random service
+// instance from the provided service name.
+func ServiceConnection(ctx context.Context, serviceName string, registry discovery.Registry) (*grpc.ClientConn, error) {
+	addrs, err := registry.ServiceAddresses(ctx, serviceName)
+	if err != nil {
+		return nil, err
+	}
+
+	return grpc.NewClient(addrs[rand.Intn(len(addrs))])
+}
