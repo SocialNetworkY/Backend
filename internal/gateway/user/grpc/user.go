d4607bc01f90d638cdc1376dec501655cc51bf0c
lapkomo2018 <lapkomo2018@gmail.com>
2024-09-15 22:03:53 +0300

    added user gateway

---
+++ internal/gateway/user/grpc/user.go
@@ -0,0 +1,15 @@
+package grpc
+
+import "github.com/lapkomo2018/goTwitterAuthService/pkg/discovery"
+
+// Gateway represents the gRPC gateway for the user service.
+type Gateway struct {
+	registry discovery.Registry
+}
+
+const UserServiceName = "user"
+
+// New creates a new Gateway.
+func New(r discovery.Registry) *Gateway {
+	return &Gateway{registry: r}
+}
