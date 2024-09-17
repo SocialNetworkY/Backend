e829603b2ea8ce0b253de827b186f1f344d19b7e
lapkomo2018 <lapkomo2018@gmail.com>
2024-09-17 12:50:43 +0300

    internal/auth/model moved content from activationToken.go, refreshToken.go to token.go

--- internal/auth/model/activationToken.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type ActivationToken struct {
-	UserID uint   `gorm:"primaryKey"`
-	Token  string `gorm:"unique"`
-}
--- internal/auth/model/token.go
+++ internal/auth/model/token.go
@@ -1,5 +1,10 @@
 package model
 
+type ActivationToken struct {
+	UserID uint   `gorm:"primaryKey"`
+	Token  string `gorm:"unique"`
+}
+
 type RefreshToken struct {
 	UserID uint   `gorm:"primaryKey"`
 	Token  string `gorm:"unique"`
