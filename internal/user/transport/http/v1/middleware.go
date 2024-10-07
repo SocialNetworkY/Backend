8e1442bf5a9fa1c7c91869fda905b237965f5f8e
lapkomo2018 <lapkomo2018@gmail.com>
2024-10-08 00:45:53 +0300

    user/http: move const authorizationHeader to pkg/constant

--- internal/user/transport/http/v1/middleware.go
+++ internal/user/transport/http/v1/middleware.go
@@ -3,21 +3,21 @@ package v1
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/lapkomo2018/goTwitterServices/internal/user/model"
+	"github.com/lapkomo2018/goTwitterServices/pkg/constant"
 	"net/http"
 	"strconv"
 )
 
 const (
-	authorizationHeader = "Authorization"
-	userLocals          = "user"
-	requesterLocals     = "requester"
-	paramUserID         = "userID"
-	paramUsername       = "username"
+	userLocals      = "user"
+	requesterLocals = "requester"
+	paramUserID     = "userID"
+	paramUsername   = "username"
 )
 
 func (h *Handler) authenticationMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		authHeader := c.Request().Header.Get(authorizationHeader)
+		authHeader := c.Request().Header.Get(constant.HTTPAuthorizationHeader)
 		if authHeader == "" {
 			return echo.NewHTTPError(http.StatusUnauthorized, "Missing Authorization header")
 		}
